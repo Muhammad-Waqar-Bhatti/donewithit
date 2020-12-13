385b95b8252c2a7c5fdb67ec6ba4f82b0c13c795
Muhammad Waqar <mohammadwaqar401@gmail.com>
2020-12-13 10:14:59 +0500

    Organized app compoenents

--- app/screen/LoginScreen.js
+++ app/screen/LoginScreen.js
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Image } from 'react-native';
-import AppButton from '../components/AppButton';
-import AppTextInput from '../components/AppTextInput';
 
-import {Formik} from 'formik';
 import * as Yup from 'yup';
 import Screen from '../components/screen';
-import AppText from '../components/AppText';
-import ErrorMessage from '../components/ErrorMessage';
-import AppFormField from '../components/AppFormField';
-import SubmitButton from '../components/SubmitButton';
-import AppForm from '../components/AppForm';
+
+import {AppForm,AppFormField,SubmitButton} from '../components/forms';
+
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
